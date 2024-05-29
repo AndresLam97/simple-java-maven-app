b46dc5d1c3a316b05dac39c069535adcc10ae0c0
AndresLam97 <79527274+AndresLam97@users.noreply.github.com>
2024-05-29 13:24:32 +0700

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -12,10 +12,12 @@ echo 'The following command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 
 NAME=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.name)
+mvn -q -DforceStdout help:evaluate -Dexpression=project.name
 
 echo 'The following command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 VERSION=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.version)
+mvn -q -DforceStdout help:evaluate -Dexpression=project.version
 
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
@@ -23,4 +25,4 @@ echo "NAME= $NAME"
 echo "VERSION= $VERSION"
 set -x
 java -jar target/$NAME-$VERSION.jar
-set +x
+
